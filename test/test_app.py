12cde55a7bfd0ae89702033fc8655494537d238f
radicaled <daniel.bianco@outlook.com>
2024-10-09 20:21:37 -0300

    remove unit test

--- test/test_app.py
+++ test/test_app.py
@@ -16,35 +16,5 @@ class FlaskTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Element written successfully!', response.data)
 
-    @patch('map_handler.MapHandler.merge_map')
-    @patch('map_handler.MapHandler.write_local_map')
-    def test_write_map(self, mock_write, mock_merge):
-        mock_merge.return_value = [1]
-        mock_write.return_value = 1
-        data = {
-            "start": "(1,1)",
-            "payload": {"type": 2, "direction": "up"}
-        }
-        response = self.app.post('/write', json=data)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Writing action completed successfully!', response.data)
-
-    @patch('map_handler.MapHandler.clean_polyverse')
-    def test_clean_polyverse(self, mock_clean):
-        response = self.app.post('/clean')
-        mock_clean.assert_called_once()
-        self.assertEqual(response.status_code, 200)
-
-    @patch('map_handler.MapHandler.duplicate_polyverse')
-    def test_duplicate_map(self, mock_duplicate):
-        mock_duplicate.return_value = [1]
-        data = {
-            "url": "https://someurl.com"
-        }
-        response = self.app.post('/duplicate', json=data)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Duplication action completed successfully!', response.data)
-
-
 if __name__ == '__main__':
     unittest.main()
--- test/test_map_handler.py
+++ test/test_map_handler.py
@@ -16,23 +16,5 @@ class MapHandlerTestCase(unittest.TestCase):
         self.api = PolyverseAPI("https://fakeurl.com", "candidate-id")
         self.map_handler = MapHandler(self.api)
 
-    def test_show_local_polyverse(self):
-        result = self.map_handler.show_local_polyverse()
-        expected_result = [['P', '-', 'S'], ['-', 'C', '-']]
-        self.assertEqual(result, expected_result)
-
-    @patch('api.PolyverseAPI.write_element')
-    def test_write_local_map(self, mock_write):
-        element_data = {"row": 0, "column": 1, "payload": {"type": 1, "color": "blue"}}
-        self.map_handler.write_local_map(element_data)
-        mock_write.assert_called_once()
-
-    @patch('api.PolyverseAPI.delete_element')
-    def test_delete_local_map(self, mock_delete):
-        element_data = {"row": 1, "column": 2, "payload": {"type": 1}}
-        self.map_handler.delete_local_map(element_data)
-        mock_delete.assert_called_once()
-
-
 if __name__ == '__main__':
     unittest.main()
